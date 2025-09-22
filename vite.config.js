fe6d8ef25c1f1b9f100a043a477ca38d64cf9a5d
FUNNYCOCO <134181758+FUNNYCOCO@users.noreply.github.com>
2025-09-22 15:52:25 +0000

    twoweb2

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig(async ({ command, mode }) => {
         input: {
           main: 'index.html',
           404: '404.html',
+          AIcode1: 'AIcode1.html', // 例如：你的 about.html 文件
+          shijian1: 'shijian1.html', // 例如：你的 contact.html 文件
         },
       }
     }
